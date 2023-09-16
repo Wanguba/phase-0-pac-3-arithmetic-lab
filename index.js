d53a143d49be91e16ed63ca657bc6bde2faa4dbd
Edith Wanguba <wanguamor@gmail.com>
2023-09-16 13:12:01 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+let number = 5;
+number += 3;
+number -= 2;
+number *= 10;
+number /= 5;
+number = 10;
+
+function add5() {
+  return (number += 5);
+}
+
+function divideBy3() {
+  return (number /= 3);
+}
+parseInt("2", 10);
+parseInt("2.2222", 10);
+parseInt("nonsense!", 10);
+parseFloat("80.123999");
